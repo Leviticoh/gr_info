519ca83c0dea0f94b1ed4a8bac4f188b48b7db18
Leviticoh <epsilon.zeta@protonmail.com>
2021-11-28 10:15:33 +0100

    added support for booleans

--- getsong.scm
+++ getsong.scm
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S guile --no-auto-compile -s
 !#
 
-(use-modules (web client) (ice-9 textual-ports) (ice-9 pretty-print))
+(use-modules (web client) (ice-9 textual-ports) (ice-9 pretty-print) (rnrs bytevectors))
 
-(define (text-dl uri) (call-with-values (lambda () (http-get uri)) (lambda (a b) b)))
+(define (text-dl uri) (utf8->string (call-with-values (lambda () (http-get uri #:decode-body? #f)) (lambda (a b) b))))
 
 (define json-whitespace (char-set #\space #\tab #\newline #\return))
 
@@ -39,6 +39,13 @@
 
 (define (json-string testo)
   (list->string (reverse (_json-string_ '() (cdr (string->list testo))))))
+
+(define (json-atom testo)
+  (cond
+    ((string=? "true" (string-take testo 4)) #t)
+    ((string=? "false" (string-take testo 5)) #f)
+    (else '())))
+
 #!
 (define (json-value testo)
   (cond
@@ -51,6 +58,20 @@
     ((char-set-contains? (char-set #\t #\f #\n) (string-ref testo 0))
      (json-atom testo))))
 !#
+
+(define (json-value testo)
+  (cond
+    ((char=? (string-ref testo 0) #\")
+     (json-string testo))
+    ((char=? (string-ref testo 0) #\{)
+     (json-object testo))
+    ((char-set-contains? (char-set #\t #\f #\n) (string-ref testo 0))
+     (json-atom testo))
+    (else
+      (string-trim-right (substring testo 0 (- (string-length testo)
+					       (string-length (json-trim-element testo))))
+			 (char-set-adjoin json-whitespace #\,)))))
+
 (define (_json-trim-name_ testo)
   (cond
 
@@ -68,23 +89,12 @@
 (define (json-trim-name testo)
   (_json-trim-name_ (string-drop testo 1)))
 
-#!
 
 (define (json-element testo)
   (cons
     (json-string testo)
     (json-value (json-trim-name testo))))
 
-!#
-
-(define (json-element testo)
-  (define coda (json-trim-name testo))
-  (cons
-    (json-string testo)
-    (cond ((char=? (string-ref coda 0) #\{) (json-object coda))
-	  (else (string-trim-right (substring coda 0 (- (string-length coda)
-				     (string-length (json-trim-element testo))))
-				   (char-set-adjoin json-whitespace #\,))))))
 
 (define (_json-trim-element_ escap paren testo)
   (cond
@@ -151,3 +161,9 @@
 (define json-parse json-object)
 
 (pretty-print (json-parse (text-dl "https://gensokyoradio.net/api/station/playing/")))
+#!
+(pretty-print (json-parse (get-string-all (current-input-port))))
+!#
+#!
+(pretty-print (json-parse (text-dl (get-line (current-input-port)))))
+!#
